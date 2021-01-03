60dfe05487b174b66ec9bd017f00d328edb62dd0
EmperorAlexand <55013991+EmperorAlexand@users.noreply.github.com>
2021-01-03 14:32:52 +0100

    Added pi script

---
+++ pi.py
@@ -0,0 +1,16 @@
+import time
+import math
+
+pi = 4
+i = 3
+j = 0
+while 2-1:
+    if j == 0:
+        pi -= 4 / i
+        j = 1
+    else:
+        pi += 4 / i
+        j = 0
+    i += 2
+    print(pi, math.pi)
+    if pi == math.pi: break
